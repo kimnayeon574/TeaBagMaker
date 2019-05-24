5bfcc5ba2af5ce32023cb6f80f988da3ea38107c
nayeon Kim <kimnayeon574@gmail.com>
2019-05-24 10:55:27 +0900

    #1 콤보박스, 버튼, 라벨, 타이머로 폼을 디자인하기

--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -9,11 +9,26 @@ using System.Windows.Forms;
 
 namespace TeaBagMaker
 {
-    public partial class Form1 : Form
+    public partial class teaForm : Form
     {
-        public Form1()
+        string[] teaBag = { "홍차", "녹차", "루이보스차", "국화차" };
+        public teaForm()
         {
             InitializeComponent();
         }
+
+        private void teaForm_Load(object sender, EventArgs e)
+        {
+            for(int i=0; i < teaBag.Length; i++)
+            {
+                this.cbBox.Items.Add(teaBag[i]);
+            }
+            if(this.cbBox.Items.Count > 0)
+            {
+                this.cbBox.SelectedIndex = 0;
+            }
+        }
+
+        
     }
 }
