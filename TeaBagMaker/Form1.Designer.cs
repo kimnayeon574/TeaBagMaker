93162321b1e61e0bf01faa67678ac8abeea9b522
nayeon Kim <kimnayeon574@gmail.com>
2019-05-24 11:03:20 +0900

    #1 콤보박스, 버튼, 라벨, 타이머를 활용하여 폼  디자인하기

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TeaBagMaker
 {
-    partial class Form1
+    partial class Form
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -29,12 +29,90 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.cbBox = new System.Windows.Forms.ComboBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.teaTime = new System.Windows.Forms.Label();
+            this.timeResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // cbBox
+            // 
+            this.cbBox.Font = new System.Drawing.Font("굴림", 16F);
+            this.cbBox.FormattingEnabled = true;
+            this.cbBox.Location = new System.Drawing.Point(12, 12);
+            this.cbBox.Name = "cbBox";
+            this.cbBox.Size = new System.Drawing.Size(363, 29);
+            this.cbBox.TabIndex = 0;
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("굴림", 20F);
+            this.btn.Location = new System.Drawing.Point(12, 57);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(363, 91);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "담그기 !";
+            this.btn.UseVisualStyleBackColor = true;
+            // 
+            // teaTime
+            // 
+            this.teaTime.AutoSize = true;
+            this.teaTime.Font = new System.Drawing.Font("굴림", 14F);
+            this.teaTime.Location = new System.Drawing.Point(12, 171);
+            this.teaTime.Name = "teaTime";
+            this.teaTime.Size = new System.Drawing.Size(65, 19);
+            this.teaTime.TabIndex = 2;
+            this.teaTime.Text = "시간 : ";
+            // 
+            // timeResult
+            // 
+            this.timeResult.AutoSize = true;
+            this.timeResult.Font = new System.Drawing.Font("굴림", 14F);
+            this.timeResult.Location = new System.Drawing.Point(12, 206);
+            this.timeResult.Name = "timeResult";
+            this.timeResult.Size = new System.Drawing.Size(15, 19);
+            this.timeResult.TabIndex = 3;
+            this.timeResult.Text = " ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("궁서", 22F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(12, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "2019.05.24 3203 김나연";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(387, 303);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timeResult);
+            this.Controls.Add(this.teaTime);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.cbBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.Text = "차 우리기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbBox;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label teaTime;
+        private System.Windows.Forms.Label timeResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
